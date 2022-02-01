a298cbfd3dcdfd6b087259da835bfa4cde73df72
FedeSpu <federico.spurio@studio.unibo.it>
2022-02-01 18:37:29 +0100

    Updated scraper and added anaconda environment file

--- src/selen.py
+++ src/selen.py
@@ -3,25 +3,68 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException
+import pandas as pd
 import time
 
 
-def get_claim_votes(stance, level):
-    claim = wd.find_element_by_class_name('selected-claim').find_element_by_class_name('claim-text__content').text
+dataset = pd.DataFrame(columns=['Level', 'Stance', 'Claim', 'Votes', 'Comments', 'Depth'])
+claim_already_visited = {}
+
+
+def try_click(elem):
+    attempts = 0
+    global actions
+    while attempts < 3:
+        try:
+            elem.click()
+            break
+        except ElementClickInterceptedException:
+            print('EXCEPTION')
+            try:
+                wd.find_element_by_class_name('pop-up-template__close').click()
+                print('popup')
+                # break
+            except Exception:
+                print('EXCEPTION2')
+            # Move mouse to another location to avoid the text shown by hovering mouse on branch claim
+            actions.move_to_element(wd.find_element_by_class_name('icon--kialo-logo')).perform()
+            time.sleep(2)
+            attempts += 1
+
+
+def get_claim_votes(stance, level, pros_map, cons_map, depth):
+    try:
+        claim = wd.find_element_by_class_name('selected-claim').find_element_by_class_name('claim-text__content').text
+    except Exception as e:
+        claim = str('Kialo discurssion: ') + wd.find_element_by_class_name('selected-claim').text
+    if claim in claim_already_visited:
+        return
+    else:
+        claim_already_visited[claim] = True
+    print(claim)
+
     claim_container = wd.find_element_by_class_name('selected-claim-container')
-    time.sleep(1)
+    time.sleep(0.2)
     WebDriverWait(claim_container, 2000).until(
-        # EC.presence_of_element_located((By.CLASS_NAME, 'claim-controls__control-item--menu-button'))
         EC.element_to_be_clickable((By.CLASS_NAME, 'claim-controls__control-item--menu-button'))
-        # EC.visibility_of_element_located((By.CLASS_NAME, 'claim-controls__control-item--menu-button'))
     )
 
-    claim_container.find_element_by_class_name('claim-controls__control-item--menu-button').click()
+    comments = claim_container.find_element_by_class_name('comments-indicator').text
+
+    button = claim_container.find_element_by_class_name('claim-controls__control-item--menu-button')
+    try_click(button)
+    try:
+        WebDriverWait(wd, 200).until(EC.visibility_of_element_located((By.CLASS_NAME, 'context-menu__item-list')))
+    except:
+        claim_container.find_element_by_class_name('claim-header__impact-container').click()
+        time.sleep(1)
+        button.click()
     context_menu = wd.find_element_by_class_name('context-menu').find_elements_by_class_name('context-menu-item__text')
 
     for opt in context_menu:
         if opt.text == 'Voting Stats':
-            opt.click()
+            try_click(opt)
             break
 
     histogram = wd.find_elements_by_class_name('voters-histogram__voters-bar-container')
@@ -29,19 +72,68 @@ def get_claim_votes(stance, level):
     for count in histogram:
         votes.append(count.find_element_by_class_name('icon__counter').text)
 
-    return [level, stance, claim, votes]
+    global dataset
+    dataset = dataset.append({'Level': level, 'Stance': stance, 'Claim': claim, 'Votes': votes, 'Comments': comments, 'Depth': depth},
+                             ignore_index=True)
+
+    new_total_pros_map = wd.find_elements_by_class_name('mini-map-claim--pro')
+    new_total_cons_map = wd.find_elements_by_class_name('mini-map-claim--con')
+    sub_pros_map = list(set(new_total_pros_map) - set(pros_map))
+    sub_cons_map = list(set(new_total_cons_map) - set(cons_map))
+
+    index_level = 1
+    for pro in sub_pros_map:
+        try_click(pro.find_element_by_tag_name('rect'))
+        get_claim_votes('pro', level + '.' + str(index_level), new_total_pros_map, new_total_cons_map, depth + 1)
+        index_level += 1
+        
+    for con in sub_cons_map:
+        try_click(con.find_element_by_tag_name('rect'))
+        get_claim_votes('con', level + '.' + str(index_level), new_total_pros_map, new_total_cons_map, depth + 1)
+        index_level += 1
 
 
 firefox_options = webdriver.FirefoxOptions()
-# firefox_options.add_argument('--headless')
+firefox_options.add_argument('--headless')
 firefox_options.add_argument('--no-sandbox')
 firefox_options.add_argument('--disable-dev-shm-usage')
+firefox_options.add_argument('--disable-notifications')
+firefox_options.add_argument('--disable-popup-blocking')
 wd = webdriver.Firefox(options=firefox_options)
 actions = ActionChains(wd)
 
-wd.get("https://www.kialo.com/pro-life-vs-pro-choice-should-abortion-be-legal-5637?closeSidebar=notifications.activity")
+'''
+SELECTION OF THE DISCUSSION
+'''
+# wd.get("https://www.kialo.com/pro-life-vs-pro-choice-should-abortion-be-legal-5637?closeSidebar=notifications.activity")
+# wd.get('https://www.kialo.com/should-conscientious-objection-to-abortion-be-banned-2851')
+# wd.get('https://www.kialo.com/should-there-be-a-universal-basic-income-ubi-1634?closeSidebar=notifications.activity')
+wd.get('https://www.kialo.com/the-existence-of-god-2629')
+# wd.get('https://www.kialo.com/high-speed-rail-network-europe-55592')
 
-main_claim = wd.find_element_by_class_name('selected-claim').text
+# Close "new to Kialo" dialog
+time.sleep(4)
+WebDriverWait(wd, 200).until(
+    EC.visibility_of_element_located((By.CLASS_NAME, 'pop-up-template__inner-modal-wrapper'))
+)
+wd.find_element_by_class_name('pop-up-template__inner-modal-wrapper').find_element_by_class_name('pop-up-template__close').click()
+
+# Close discussion dialog
+time.sleep(4)
+WebDriverWait(wd, 200).until(
+    EC.visibility_of_element_located((By.CLASS_NAME, 'pop-up-template__body'))
+)
+wd.find_element_by_class_name('pop-up-template__body').find_element_by_class_name('discussion-info-dialog__close').click()
+
+# Close cookies dialog
+time.sleep(5)
+WebDriverWait(wd, 2000).until(
+    EC.element_to_be_clickable((By.CLASS_NAME, 'toast-template__close-icon'))
+)
+wd.find_element_by_class_name('toast-template__close-icon').click()
+
+
+main_claim = wd.find_element_by_class_name('selected-claim').find_element_by_class_name('claim-text__content').text
 WebDriverWait(wd, 2000).until(
     EC.element_to_be_clickable((By.CLASS_NAME, 'claim-controls__control-item--menu-button'))
 )
@@ -60,63 +152,23 @@ for count in histogram:
 
 print(str(main_claim) + str(votes))
 
+dataset = dataset.append({'Level': '1', 'Stance': 'None', 'Claim': main_claim, 'Votes': votes, 'Depth': 0}, ignore_index=True)
 
 pros_map = wd.find_elements_by_class_name('mini-map-claim--pro')
 cons_map = wd.find_elements_by_class_name('mini-map-claim--con')
-# print(len(pros_map))
-# print(len(cons_map))
 
-index_level = 0
+index_level = 1
 for pro_map in pros_map:
     pro_map.find_element_by_tag_name('rect').click()
-    res = get_claim_votes('pro', '1.{}'.format(index_level+1))
-    print(res)
+    get_claim_votes('pro', '1.{}'.format(index_level), pros_map, cons_map, 1)
     index_level += 1
 
 for con_map in cons_map:
     con_map.find_element_by_tag_name('rect').click()
-    res = get_claim_votes('con', '1.{}'.format(index_level+1))
-    print(res)
+    get_claim_votes('con', '1.{}'.format(index_level), pros_map, cons_map, 1)
     index_level += 1
-'''
-pro_map[0].find_element_by_tag_name('rect').click()
-pro_map2 = wd.find_elements_by_class_name('mini-map-claim--pro')
-con_map2 = wd.find_elements_by_class_name('mini-map-claim--con')
-print(len(set(pro_map2) - set(pro_map)))
-print(len(con_map))
-claim = wd.find_element_by_class_name('selected-claim').text
-print(claim)
-'''
-'''
-# el = WebDriverWait(wd, 2000).until(EC.invisibility_of_element((By.CLASS_NAME, 'pre-rendered')))
-f = open('page.txt', 'w')
-f.write(wd.page_source)
-f.close()
-'''
-'''
-el = WebDriverWait(wd, 5).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="scroll-context-20"]/div[4]/div/div[2]/div/main/div[1]/div/div/div/div[1]/div[2]/div/div/div/div/div[1]/div[1]/div[3]/button[1]'))
-)
-# wd.find_element_by_xpath('//*[@id="scroll-context-20"]/div[4]/div/div[2]/div/main/div[1]/div/div/div/div[1]/div[2]/div/div/div/div/div[1]/div[1]/div[3]/button[1]')
 
-claim_cont = wd.find_element_by_class_name('selected-claim-container')
-claim_controls = wd.find_element_by_class_name('claim-header__controls')
-wd.switch_to.(claim_controls)
-# print(claim_controls.find_elements_by_name('*'))
-butt = claim_controls.find_elements_by_tag_name('button')
-print(butt)
+dataset.to_excel('output.xlsx')
+print(dataset.iloc[:-1])
 
-print(wd.find_element_by_class_name('impact-representation__foreground').get_attribute('style'))
-
-# actions.context_click(claim_cont).perform()
-
-# el = wd.find_element_by_class_name('claim-text__content')
-# print(el.text)
-
-hoverable-tooltip-component icon-button claim-controls__control-item claim-controls__control-item--menu-button icon-hover-and-active-trigger
-hoverable-tooltip-component icon-button claim-controls__control-item comments-indicator                        icon-hover-and-active-trigger
-
-hoverable-tooltip-component icon-button claim-controls__control-item claim-controls__control-item--menu-button icon-hover-and-active-trigger
-hoverable-tooltip-component icon-button claim-controls__control-item claim-controls__control-item--menu-button icon-hover-and-active-trigger
-'''
 wd.close()
